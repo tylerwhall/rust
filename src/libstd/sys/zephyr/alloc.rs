cdccb85f917bdef7145ac87f8cbb12b48d83af41
Tyler Hall <tylerwhall@gmail.com>
2019-05-29 14:48:43 -0400

    zephyr: alloc: pass through to k_malloc

--- src/libstd/sys/zephyr/alloc.rs
+++ src/libstd/sys/zephyr/alloc.rs
@@ -4,21 +4,21 @@ use crate::alloc::{GlobalAlloc, Layout, System};
 unsafe impl GlobalAlloc for System {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        unimplemented!()
+        ::zephyr::kernel::KMalloc.alloc(layout)
     }
 
     #[inline]
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        unimplemented!()
+        ::zephyr::kernel::KMalloc.alloc_zeroed(layout)
     }
 
     #[inline]
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        unimplemented!()
+        ::zephyr::kernel::KMalloc.dealloc(ptr, layout)
     }
 
     #[inline]
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        unimplemented!()
+        ::zephyr::kernel::KMalloc.realloc(ptr, layout, new_size)
     }
 }
