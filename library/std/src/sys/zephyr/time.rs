c55114d0c1058a054fb76a87426b039c183d2c09
Tyler Hall <tylerwhall@gmail.com>
2019-05-31 18:52:29 -0400

    zephyr: implement Instant with InstantMs backdoor
    In zephyr_core, we can implement a more efficient direct conversion from
    the delta of two instants to a i32 delta in ms, which Zephyr uses for
    timeouts. Instants are already in ms, so it's just a 64-bit subtraction,
    cutting out the bidirectional conversion to Duration which requires
    64-bit multiplication and division.

--- library/std/src/sys/zephyr/time.rs
+++ library/std/src/sys/zephyr/time.rs
@@ -1,7 +1,8 @@
 use crate::time::Duration;
+use zephyr_core::Ticks;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
-pub struct Instant(Duration);
+pub struct Instant(pub Ticks);
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
 pub struct SystemTime(Duration);
@@ -10,27 +11,27 @@ pub const UNIX_EPOCH: SystemTime = SystemTime(Duration::from_secs(0));
 
 impl Instant {
     pub fn now() -> Instant {
-        unimplemented!()
+        Instant(zephyr_core::any::k_uptime_ticks())
     }
 
     pub const fn zero() -> Instant {
-        Instant(Duration::from_secs(0))
+        Instant(Ticks(0))
     }
 
     pub fn actually_monotonic() -> bool {
-        false // ?
+        true
     }
 
     pub fn checked_sub_instant(&self, other: &Instant) -> Option<Duration> {
-        self.0.checked_sub(other.0)
+        self.0.checked_sub(other.0).map(Into::into)
     }
 
     pub fn checked_add_duration(&self, other: &Duration) -> Option<Instant> {
-        Some(Instant(self.0.checked_add(*other)?))
+        self.0.checked_add_duration(other).map(Instant)
     }
 
     pub fn checked_sub_duration(&self, other: &Duration) -> Option<Instant> {
-        Some(Instant(self.0.checked_sub(*other)?))
+        self.0.checked_sub_duration(other).map(Instant)
     }
 }
 
