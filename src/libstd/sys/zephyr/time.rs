2e47b183b25acd478d2edca0a1ef32cddc86d989
Tyler Hall <tylerwhall@gmail.com>
2019-05-31 18:52:29 -0400

    zephyr: implement Instant

--- src/libstd/sys/zephyr/time.rs
+++ src/libstd/sys/zephyr/time.rs
@@ -1,7 +1,8 @@
 use crate::time::Duration;
+use zephyr::InstantMs;
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
-pub struct Instant(Duration);
+pub struct Instant(InstantMs);
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
 pub struct SystemTime(Duration);
@@ -10,27 +11,27 @@ pub const UNIX_EPOCH: SystemTime = SystemTime(Duration::from_secs(0));
 
 impl Instant {
     pub fn now() -> Instant {
-        unimplemented!()
+        Instant(zephyr::any::k_uptime_get_ms())
     }
 
     pub const fn zero() -> Instant {
-        Instant(Duration::from_secs(0))
+        Instant(InstantMs::zero())
     }
 
     pub fn actually_monotonic() -> bool {
-        false // ?
+        true
     }
 
     pub fn checked_sub_instant(&self, other: &Instant) -> Option<Duration> {
-        self.0.checked_sub(other.0)
+        self.0.checked_sub(other.0).map(Into::into)
     }
 
     pub fn checked_add_duration(&self, other: &Duration) -> Option<Instant> {
-        Some(Instant(self.0.checked_add(*other)?))
+        self.0.checked_add_duration(other).map(Instant)
     }
 
     pub fn checked_sub_duration(&self, other: &Duration) -> Option<Instant> {
-        Some(Instant(self.0.checked_sub(*other)?))
+        self.0.checked_sub_duration(other).map(Instant)
     }
 }
 
