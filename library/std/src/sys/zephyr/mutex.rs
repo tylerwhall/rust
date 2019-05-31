af6d1dcd08a0838ff97afb8fe4f15100384a9a5e
Tyler Hall <tylerwhall@gmail.com>
2019-05-31 14:27:55 -0400

    zephyr: implement mutex
    This has an extra allocation in the kernel mode case, but it allows the
    same code to be compatible with both kernel and user mode. DynMutex will
    alloc from CONFIG_RUST_MUTEX_POOL to support user mode.

--- library/std/src/sys/zephyr/mutex.rs
+++ library/std/src/sys/zephyr/mutex.rs
@@ -1,34 +1,55 @@
-pub struct Mutex {}
+use core::cell::UnsafeCell;
+use core::mem::MaybeUninit;
+use core::ptr;
+
+use zephyr_core::context::Any as Context;
+use zephyr_core::mutex::*;
+use zephyr_core::mutex_alloc::DynMutex;
+
+pub type MovableMutex = Mutex;
+
+pub struct Mutex(UnsafeCell<MaybeUninit<DynMutex>>);
 
 unsafe impl Send for Mutex {}
 unsafe impl Sync for Mutex {}
 
 impl Mutex {
+    #[rustc_const_stable(feature = "const_sys_mutex_new", since = "1.0.0")]
     pub const fn new() -> Mutex {
-        Mutex {}
+        Mutex(UnsafeCell::new(MaybeUninit::uninit()))
+    }
+
+    unsafe fn as_ref(&self) -> &DynMutex {
+        &*(*self.0.get()).as_ptr()
+    }
+
+    unsafe fn as_mut_ptr(&self) -> *mut DynMutex {
+        (*self.0.get()).as_mut_ptr()
     }
 
     #[inline]
     pub unsafe fn init(&self) {
+        *self.0.get() = MaybeUninit::new(DynMutex::new::<Context>().expect("mutex alloc"))
     }
 
     #[inline]
     pub unsafe fn lock(&self) {
-        unimplemented!()
+        self.as_ref().lock::<Context>()
     }
 
     #[inline]
     pub unsafe fn unlock(&self) {
-        unimplemented!()
+        self.as_ref().unlock::<Context>()
     }
 
     #[inline]
     pub unsafe fn try_lock(&self) -> bool {
-        unimplemented!()
+        self.as_ref().try_lock::<Context>()
     }
 
     #[inline]
     pub unsafe fn destroy(&self) {
+        ptr::drop_in_place(self.as_mut_ptr());
     }
 }
 
@@ -36,7 +57,7 @@ pub struct ReentrantMutex {}
 
 impl ReentrantMutex {
     pub const fn uninitialized() -> ReentrantMutex {
-        ReentrantMutex { }
+        ReentrantMutex {}
     }
 
     #[inline]
